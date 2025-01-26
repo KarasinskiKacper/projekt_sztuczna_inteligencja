15a420471f69a9660c9a067d5b58981bde31a7cd
KarasinskiKacper <karasinski.kacper@outlook.com>
2025-01-26 23:48:09 +0100

    update generate_db

--- README.md
+++ README.md
@@ -9,7 +9,9 @@
 <h2>O projekcie</h2>
 W projekcie została stworzona aplikacja webowa wykorzystująca bibliotekę Flask, w której zaimplementowano modele konwolucyjnych sieci neuronowych do rozpoznawania tekstu na obrazach. Po przesłaniu pliku ze zdjęciem można wybrać 1 z 4 przygotowanych modeli do rozpoznania tekstu. Następnie po kliknięciu "Wyślij", aplikacja wykona analizę zdjęcia wybranym modelem i wypisze na ekranie rozpoznany tekst. W celu trenowania modeli, została wygenerowana baza danych zawierająca 5mln zdjęć z polskimi słowami (skrypt do wygenerowania bazy danych - ./generate_db/generate_data.py).
 
-W projekcie został wykorzystany gotowy model służący do znajdywania położenia tekstu na zdjęciu: [CRAFT-pytorch](https://github.com/clovaai/CRAFT-pytorch)
+Zdjęcia ze słowami były generowane z wykorzystaniem repozytorium: [text_renderer](https://github.com/oh-my-ocr/text_renderer).
+
+W projekcie został wykorzystany gotowy model służący do znajdywania położenia tekstu na zdjęciu: [CRAFT-pytorch](https://github.com/clovaai/CRAFT-pytorch).
 
 <h2>Wymagania</h2>
 <h3>Aplikacja webowa</h3>
@@ -38,6 +40,10 @@ serwer aplikacji webowej wystartuje pod adresem http://127.0.0.1:5000
 git clone https://github.com/KarasinskiKacper/projekt_sztuczna_inteligencja.git [nazwa folderu]
 cd [nazwa folderu]
 cd generate_db
+git clone https://github.com/oh-my-ocr/text_renderer.git text_renderer
+cd text_renderer
+pip install -e .
+cd ..
 pip install -r requirements.txt
 python generate_data.py
 ```
--- generate_db/generate_data.py
+++ generate_db/generate_data.py
@@ -1,141 +1,132 @@
-import numpy as np
-from torch.utils.data import Dataset
-from PIL import Image, ImageDraw, ImageFont
-from torchvision.transforms import v2
 import os
+import lmdb
+from PIL import Image
+from tqdm import tqdm
+import sys
+import cv2
+import numpy as np
+import logging
+from pathlib import Path
+from text_renderer.render import Render
+from text_renderer.config import (
+    RenderCfg,
+    GeneratorCfg,
+    FixedTextColorCfg,
+    Effects,
+    NormPerspectiveTransformCfg
+)
+from text_renderer.corpus import WordCorpus, WordCorpusCfg
+from text_renderer.effect import DropoutRand
+
+# ============== CONFIGURATION ==============
+TEXT_PATHS = [Path("./words.txt")]
+BG_DIR = Path("./backgrounds")
+FONT_DIR = Path("./fonts")
+CHARS_FILE = Path("./chars.txt")
+LMDB_FULL_PATH = os.path.normpath("./dataset.lmdb")
+MAP_SIZE =   64*2**30  # 64GB 
+BATCH_SIZE = 500 
+# ===========================================
+
+def create_lmdb_directory():
+    lmdb_dir = os.path.dirname(LMDB_FULL_PATH)
+    
+    if not os.path.exists(lmdb_dir):
+        print(f"Creating directory: {lmdb_dir}")
+        try:
+            os.makedirs(lmdb_dir, exist_ok=True)
+            if not os.path.isdir(lmdb_dir):
+                raise NotADirectoryError(f"Failed to create directory: {lmdb_dir}")
+            print("Directory successfully created")
+        except Exception as e:
+            print(f"Directory creation failed: {str(e)}")
+            sys.exit(1)
+
+def initialize_lmdb():
+    return lmdb.open(LMDB_FULL_PATH,
+                    map_size=MAP_SIZE,
+                    max_dbs=3,
+                    meminit=False,
+                    map_async=True,
+                    writemap=True,
+                    lock=False,
+                    create=True,
+                    subdir=True,
+                    )
 
 
-class PolishWordsDataset(Dataset):
-    def __init__(self, dir):
-        self.samples= list()
-        self.max_len = None
+def generate_dataset():
+    env = initialize_lmdb()
+    
+    image_db = env.open_db(b'images')
+    label_db = env.open_db(b'labels')
+    meta_db = env.open_db(b'meta')
 
-        excluded_signs = ['ä', 'ô', 'í', 'é', 'š', 'ø', 'è', 'ö', 'î', 'ë', 'ü', 'à', 'ê', 'ş', 'ç', 'č', 'ū', 'ú', 'û', 'á', 'â', 'ñ', 'ù', 'ō', 'Ä', 'Ô', 'Í', 'É', 'Š', 'Ø', 'È', 'Ö', 'Î', 'Ë', 'Ü', 'À', 'Ê', 'Ş', 'Ç', 'Č', 'Ū', 'Ú', 'Û', 'Á', 'Â', 'Ñ', 'Ù', 'Ō']
+    print("\nGenerating images...")
 
-        tmp_set = set()
-        with open(dir, 'r', encoding="utf-8") as file:
-            lines = file.readlines()
-            for line in lines:
-                sentences = line.split(",")
-                for sentence in sentences:
-                    words = sentence.split(" ")
-                    for word in words:
-                        if not any(sign in word for sign in excluded_signs) and len(word) < 21:                            
-                            tmp_set.add(word.strip())
+    charset = []
+    with open(CHARS_FILE, "r", encoding="utf8") as f:
+        charset = [line.strip() for line in f]
+    
+    with env.begin(write=True) as txn:
+        txn.put(b'charset', ''.join(charset).encode(), db=meta_db)
 
-        self.samples = list(tmp_set)
+    config = GeneratorCfg(
+        num_image=5_000_000,
+        render_cfg=RenderCfg(
+            bg_dir=BG_DIR,
+            height=64,
+            corpus=WordCorpus(
+                WordCorpusCfg(
+                    text_paths=TEXT_PATHS,
+                    font_dir=FONT_DIR,
+                    font_size=(16,64),
+                    chars_file=CHARS_FILE,
+                    filter_by_chars=False,
+                    filter_font=True,  
+                    num_word=(1, 2), 
+                )
+            ),
+            corpus_effects=Effects([DropoutRand(0.2, (0.05,0.2))]),  
+            perspective_transform=NormPerspectiveTransformCfg(scale=0.6),
+            text_color_cfg=FixedTextColorCfg(),
+            gray=True
+        )
+    )
+    render = Render(config.render_cfg)
 
-        char_set = set()
-        for sample in self.samples:
-            for char in sample:
-                char_set.add(char)
 
-        chars = list(char_set)
-        self.char_to_idx = {char: idx + 1 for idx, char in enumerate(chars)}
-        self.char_to_idx['<blank>'] = 0
-        self.idx_to_char = {idx: char for char, idx in self.char_to_idx.items()}
+    txn = env.begin(write=True) 
+    for idx in tqdm(range(config.num_image), desc="Progress"):
+        image, label = render()
+
+        image = cv2.convertScaleAbs(image,alpha=1.1)
+        if np.random.randint(0,2):
+            image = cv2.bitwise_not(image)
+        image = cv2.blur(image, np.random.randint(1,10,2), cv2.BORDER_DEFAULT)  
+        image = cv2.resize(image, (256,32))
             
+        txn.put(f"img_{idx}".encode(), Image.fromarray(image).tobytes(), db=image_db)
+        txn.put(f"label_{idx}".encode(), label.encode(), db=label_db)
+        txn.put(f"meta_{idx}".encode(), "256,32".encode(), db=meta_db)
 
-    def __len__(self):
-        if self.max_len is None:
-            return len(self.samples)
-        else:
-            return self.max_len
-        
-    def __iter__(self):
-        self.index = 0
-        return self
-
-    def __next__(self):
-        if self.index >= len(self):
-            raise StopIteration
-        else:
-            item = self.__getitem__(self.index)
-            self.index += 1
-            return item
-        
-    def create_image(self, text):
-        fonts = [
-                "digital/Inter/Inter-Italic-VariableFont_opsz,wght.ttf",
-                "digital/Inter/Inter-VariableFont_opsz,wght.ttf",
-                "digital/Montserrat/Montserrat-VariableFont_wght.ttf",
-                "digital/Montserrat/Montserrat-Italic-VariableFont_wght.ttf",
-                "digital/Noto_Sans/NotoSans-VariableFont_wdth,wght.ttf",
-                "digital/Noto_Sans/NotoSans-Italic-VariableFont_wdth,wght.ttf",
-                "digital/Open_Sans/OpenSans-VariableFont_wdth,wght.ttf",
-                "digital/Open_Sans/OpenSans-Italic-VariableFont_wdth,wght.ttf",
-                "digital/Oswald/Oswald-VariableFont_wght.ttf",
-                "digital/Roboto/Roboto-VariableFont_wdth,wght.ttf",
-                "digital/Roboto/Roboto-Italic-VariableFont_wdth,wght.ttf",
-                "digital/Roboto_Mono/RobotoMono-VariableFont_wght.ttf",
-                "digital/Roboto_Mono/RobotoMono-Italic-VariableFont_wght.ttf",
-                ]
-        
-        font = ImageFont.truetype(os.path.join("./fonts", np.random.choice(fonts)),size=np.random.randint(16,20))
-        font.set_variation_by_name(np.random.choice(font.get_variation_names()))
-
-        bbox = font.getbbox(text)
-        color = np.random.randint(200,255)
-        if color >= 127:
-            inverse = np.random.randint(0,25)
-        else:
-            inverse = np.random.randint(225,255)
-  
-        
-        # img = Image.new('L', (bbox[2] + margin[0] + margin[2], bbox[3] - bbox[1] + margin[1] + margin[3]), color=color)
-        img = Image.new('L', (300,60), color=color)
-        draw = ImageDraw.Draw(img)
-        
-        # draw.text((margin[0], margin[1]-bbox[1]), text, font=font, fill=inverse)
-        draw.text(((300  - (bbox[2]))/2, (60 - (bbox[3] - bbox[1]))/2), text, font=font, fill=inverse)
-        
-        transform = v2.Compose([
-            v2.RandomRotation(3),
-            v2.CenterCrop((32,256)),
-            # v2.ColorJitter(contrast = (1.5,1.5))
-        ])
-
-        img = transform(img)
-        
-        return img
-        
-
-    def __getitem__(self, index):
-        text = self.samples[index]
-        image = self.create_image(text)
-
-        # target = torch.tensor([self.char_to_idx[c] for c in text], dtype=torch.long)
-
-        return image, text
-       
-    
-    def shuffle(self):
-        np.random.shuffle(self.samples)
-
-    def limit(self, value):
-        self.max_len = value
-        
-        
-dataset = PolishWordsDataset("./odm.txt")
-dataset.shuffle()
-# labels = ""
-# print(len(dataset))
+        if idx % BATCH_SIZE == 0 and idx > 0:
+            txn.commit()
+            env.sync()
+            txn = env.begin(write=True)
 
 
-for i, (img, label) in enumerate(dataset):
-    # labels += f"{label}\n"
-    if i%10_000 == 0:
-        print(f"\r{i/1_000_000}", end="")
-        if i >1_000_000:
-            break
-    with open("C:/Users/karas/programowanie/uczenie maszynowe/digital_data/labels.txt", 'a', encoding="utf-8") as file:
-        file.write(f"{label}\n")
-    img.save(f"C:/Users/karas/programowanie/uczenie maszynowe/digital_data/{i}.png")
-    
-    
-    
-# print(dataset.samples[0])
-# print(len([i for i in dataset.samples if len(i)<21]))
-    # 5% 13min 13%
-# with open("./Tutorials/08_handwriting_recognition_torch/data/labels.txt", 'w', encoding="utf-8") as file:
-#     file.write(labels)
+    txn.commit()
+    env.close()
+    return
+
+if __name__ == "__main__":
+    try:
+        print(f"Target LMDB path: {LMDB_FULL_PATH}")
+        create_lmdb_directory()
+        generate_dataset()        
+        print(f"LMDB created at:\n{LMDB_FULL_PATH}")
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
